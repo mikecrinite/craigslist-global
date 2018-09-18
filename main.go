3187761bd325a97398ecee4cf2eef901566aed41
mikecrinite <mikecrinite@gmail.com>
2018-09-18 19:13:18 -0400

    Adds initial category map and region list

--- main.go
+++ main.go
@@ -8,7 +8,29 @@ import (
 )
 
 func main() {
-	fmt.Println(byteArrayAsString(executeRequest("https://philadelphia.craigslist.org/search/cto?query=subaru+impreza")))
+	url := buildURL(region[32], categoryMap["cars & trucks - by owner"], "subaru+impreza")
+	//resp := executeRequest(url)
+	//strResp := byteArrayAsString(resp)
+	fmt.Println(url)
+}
+
+var scheme = "https://"
+var base = ".craigslist.org"
+var search = "/search/"
+var query = "?query="
+
+func buildURL(region string, category string, keywords string) string {
+	if &region == nil {
+		log.Fatal("Region cannot be nil")
+	}
+	if &category == nil {
+		log.Fatal("Category cannot be nil")
+	}
+	if &keywords == nil {
+		log.Fatal("Keywords cannot be nil")
+	}
+	url := scheme + region + base + search + category + query + keywords
+	return url
 }
 
 func executeRequest(url string) []byte {
@@ -26,4 +48,116 @@ func executeRequest(url string) []byte {
 
 func byteArrayAsString(arr []byte) string {
 	return string(arr[:])
+}
+
+var categoryMap = map[string]string{
+	"apts broker fee":                     "fee",
+	"apts broker no fee":                  "nfb",
+	"apts/housing for rent":               "apa",
+	"office & commercial":                 "off",
+	"real estate - by broker":             "reb",
+	"vacation rentals":                    "vac",
+	"accounting/finance":                  "acc",
+	"admin/office":                        "ofc",
+	"architect/engineer/cad":              "egr",
+	"art/media/design":                    "med",
+	"business/mgmt":                       "bus",
+	"customer service":                    "csr",
+	"education/teaching":                  "edu",
+	"et cetera":                           "etc",
+	"food/beverage/hospitality":           "fbh",
+	"general labor":                       "lab",
+	"government":                          "gov",
+	"healthcare":                          "hea",
+	"human resource":                      "hum",
+	"legal/paralegal":                     "lgl",
+	"manufacturing":                       "mnu",
+	"marketing/advertising/pr":            "mar",
+	"nonprofit":                           "npo",
+	"real estate":                         "rej",
+	"retail/wholesale":                    "ret",
+	"sales":                               "sls",
+	"salon/spa/fitness":                   "spa",
+	"science/biotech":                     "sci",
+	"security":                            "sec",
+	"skilled trades/artisan":              "trd",
+	"software/qa/dba/etc":                 "sof",
+	"systems/networking":                  "sad",
+	"technical support":                   "tch",
+	"transportation":                      "trp",
+	"tv/film/video/radio":                 "tfr",
+	"web/html/info design":                "web",
+	"writing/editing":                     "wri",
+	"appliances - by dealer":              "ppd",
+	"atvs, utvs, snowmobiles - by dealer": "snd",
+	"auto parts - by dealer":              "ptd",
+	"auto wheels & tires - by dealer":     "wtd",
+	"boats - by dealer":                   "bod",
+	"business/commercial - by dealer":     "bfd",
+	"cars & trucks - by dealer":           "ctd",
+	"cars & trucks - by owner":            "cto",
+	"cell phones - by dealer":             "mod",
+	"computers - by dealer":               "syd",
+	"electronics - by dealer":             "eld",
+	"farm & garden - by dealer":           "grq",
+	"furniture - by dealer":               "fud",
+	"general for sale - by dealer":        "fod",
+	"heavy equipment - by dealer":         "hvd",
+	"household items - by dealer":         "hsd",
+	"materials - by dealer":               "mad",
+	"motorcycles/scooters - by dealer":    "mcd",
+	"rvs - by dealer":                     "rvd",
+	"tickets - by dealer":                 "tid",
+	"trailers - by dealer":                "trd",
+}
+
+var region = []string{
+	"albany",
+	"allentown",
+	"altoona",
+	"annapolis",
+	"baltimore",
+	"binghamton",
+	"catskills",
+	"cnj",
+	"charlottesville",
+	"chambersburg",
+	"delaware",
+	"newlondon",
+	"easternshore",
+	"martinsburg",
+	"elmira",
+	"fingerlakes",
+	"frederick",
+	"fredericksburg",
+	"harrisburg",
+	"harrisonburg",
+	"hartford",
+	"hudsonvalley",
+	"ithaca",
+	"jerseyshore",
+	"lancaster",
+	"longisland",
+	"newhaven",
+	"newyork",
+	"norfolk",
+	"newjersey",
+	"nwct",
+	"oneonta",
+	"philadelphia",
+	"poconos",
+	"reading",
+	"richmond",
+	"scranton",
+	"southjersey",
+	"smd",
+	"pennstate",
+	"syracuse",
+	"utica",
+	"washingtondc",
+	"westernmass",
+	"westmd",
+	"williamsport",
+	"winchester",
+	"york",
 }
